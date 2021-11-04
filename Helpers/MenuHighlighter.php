f37c74652ca51fdb97fdf324c3c0d6a63af93abc
Aggestor <aggeer.aa@gmail.com>
2021-11-04 08:03:09 +0100

    Menu hightlight switcher

---
+++ Helpers/MenuHighlighter.php
@@ -0,0 +1,55 @@
+<?php
+    namespace App\Helpers;
+
+    class MenuHighlighter{
+        public static array $paths = ['home','packages', 'register', 'login','products', 'services'];
+        public static string $current_path = '';
+
+        public static function get_path(){
+            if(isset($_GET['path'])){
+                $path = $_GET['path'];
+
+                if(in_array($path, Self::$paths)){
+                    switch($path){
+                        case "packages":
+                            Self::set_path("packs");
+                            break;
+                        case "products" :
+                            Self::set_path("produits");
+                            break;
+                        case "services" :
+                            Self::set_path("services");
+                            break;
+                        case "home" :
+                            Self::set_path("acceuil");
+                            break;
+                    }
+                }else{
+                    Self::set_path("404");
+                }
+            }else{
+                Self::set_path("acceuil");
+            }
+            return  new MenuHighlighter;
+        }
+        private static function set_path( string $path = ""){
+            if(is_string($path)){
+                Self::$current_path = $path;
+            }else{
+                http_response_code(404);
+                die("Wrong path provided !");
+            }
+
+        }
+         public static function high_light(string $menu = ""){
+             if(strtolower($menu) === Self::$current_path){
+                 return "_green_text";
+             }
+             else return;
+         }
+
+    }
+
+
+
+?>
