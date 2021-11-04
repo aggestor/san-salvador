dd469ece2cb14142316f128115e1c05a04a6d659
Aggestor <aggeer.aa@gmail.com>
2021-11-04 16:07:18 +0100

    redesigned landing one

--- includes/LandingOne.php
+++ includes/LandingOne.php
@@ -1,22 +1,22 @@
-<div class="col-span-12 h-auto">
-    <div class="flex w-11/12 mx-auto justify-around">
+<div class="col-span-12">
+    <div class="flex w-11/12 h-screen items-center mx-auto justify-around">
     <div class="w-6/12 flex justify-center">
         <img src="assets/logos/work.png" class="h-96 w-96" alt="Joining image" />
     </div>
-    <div class="w-6/12 flex flex-col ">
-        <h1 class=" w-11/12 text-6xl text-white font-bold">REJOIGNEZ LA RÉVOLUTION DU COMMERCE DE LA CRYPTOMONNAIE AVEC
+    <div class="w-6/12 flex flex-col mt-6 ">
+        <h1 class=" w-11/12 text-2xl text-white font-bold">REJOIGNEZ LA RÉVOLUTION DU COMMERCE DE LA CRYPTOMONNAIE AVEC
         <br> <span class="_green_text">UNIVERSAL SALVAGE TRADE (Usalvagetrade)</span></h1>
         <p class="w-8/12 text-lg mt-6 text-gray-400">Il s'agit d'un protocole de taux d'intérêt algorithmique et autonome conçu pour que les
         investisseurs débloquent un univers de contrats à terme ouverts sur les bénéfices.
         Usalvagetrade est conçu pour offrir de multiples opportunités et avantages à ses investisseurs,
         commerçants et utilisateurs afin de générer des bénéfices quotidiennement.</p>
-        <div class="flex w-6/12 mt-4 justify-between">
+        <div class="flex w-6/12 mt-4 mb-4 justify-between">
              <span>
-                <a class=" text-white font-semibold" href="/register">Créer un compte </a>
+                <a class=" text-white font-semibold" href="register">Créer un compte </a>
             </span>
             <span class="text-gray-400">ou</span>
             <span>
-                <a class="_green_btn" href="/login">Connexion</a>
+                <a class="_green_btn" href="login">Connexion</a>
             </span>
         </div>
     </div>
