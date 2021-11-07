f619ed17d7409bcf6b9e0315a86d419f39c5031d
mak098 <sergemakasi098@gmail.com>
2021-11-07 04:43:38 -0800

    method verfiy for verify what mothod are called in the action on class AdminController

--- App/Controllers/adminController.php
+++ App/Controllers/adminController.php
@@ -5,7 +5,7 @@
     use Root\App\Controllers\Generate;
     if(isset($_POST['Action'])){
         class AdminController extends adminModel{
-            public function add(){
+            static function add(){
                 $uuid=new Generate;
                 $id= $uuid->uuid();
                 $name=htmlspecialchars($_POST['name']);
@@ -14,21 +14,23 @@
                 $validatotion=new Validator();
                 if($validatotion->isString($name)){
                     if(($password==$confirmPassword)&&$validatotion->isNotEmpty($name)){
-                        while($this->checkId([$id])!=0){
+                        $admin=new AdminModel();
+                        while($admin->checkId([$id])!=0){
                             $id= $uuid->uuid();
                         }
-                        if($this->insert([$id,$name,$password,`now()`,`now()`])){
+                        if($admin->insert([$id,$name,$password,`now()`,`now()`])){
                             echo json_encode(["type"=>"success","message"=>"Enregistrement effectuer"]);
                         }else{echo json_encode(["type"=>"Failure","message"=>"Echec d'enregistrement"]);}
                     }else{echo json_encode(["type"=>"Failure","message"=>"Les mot de pass sont pas identique"]);}                
                 }else{echo json_encode(["type"=>"Failure","message"=>"Le nom doit être un texte et non nul"]);}
             }
-            public function signin(){
+            static function signin(){
                 $adminName=htmlspecialchars($_POST['adminName']);
                 $password=htmlspecialchars(sha1($_POST['password']));
                 if($adminName){
                     if($password){
-                        $getUser=$this->login([$adminName]);
+                        $admin=new AdminModel();
+                        $getUser=$admin->login([$adminName]);
                         if($getUser[0]==0){
                             echo json_encode(["type"=>"Failure","message"=>"Idendifiant incorrect"]);
                         }else{
@@ -45,6 +47,21 @@
                     }else{echo json_encode(["type"=>"Failure","message"=>"Veillez donné votre mot de passe"]);}
                 }else{echo json_encode(["type"=>"Failure","message"=>"Veillez donné votre pseudo"]);} 
             }
+            static  function verifyAction(){
+                $postAction = htmlspecialchars($_POST['action']);
+                switch ($postAction) {
+                  case 'add':
+                    AdminController::add();
+                    break;
+                  case 'signin':
+                    AdminController::signin();
+                    break;                  
+                  default:
+                    # code...
+                    break;
+                }
+              }
         }
+        AdminController::verifyAction();
     }
 ?>
