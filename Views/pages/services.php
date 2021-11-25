02185c584790b19d597a5577f4bf754b8acc3abd
Aggestor <aggeer.aa@gmail.com>
2021-11-25 12:23:34 +0100

    added services cards

--- Views/pages/services.php
+++ Views/pages/services.php
@@ -1,6 +1,7 @@
 <div class="col-span-12 bg-secondary">
-    <div class="flex flex-col md:flex-row w-11/12 md:h-screen h-auto md:items-center mx-auto md:justify-around">
-    <div data-aos="fade-down"
+    <div class="flex flex-col w-11/12 mx-auto  h-auto md:items-center">
+    <div class="flex w-full flex-col sm:flex-row md:items-center mx-auto md:justify-around">
+        <div data-aos="fade-down"
     data-aos-duration="1500" class="md:w-6/12 w-full overflow-x-hidden flex justify-center">
         <img src="assets/logos/join-free.png" class="md:h-96 h-80 w-80 md:w-96" alt="Joining image" />
     </div>
@@ -9,25 +10,101 @@
         <p class="text-gray-400 my-3 _space_text_l3 w-11/12 ">Étant prestataire de services sur les actifs          numériques, usalvagetrade, est une entreprise de
                 commerce et d'investissement en crypto-monnaies (Bitcoin, Ethereum, Dogcoin, etc)
                 qui joue l'intermédiaire en proposant les différents services relatifs à l’investissement en
-                crypto-actifs:
-                    <ul class="disc _space_text_l3 text-gray-400">
-                        <li>La conservation de crypto-actifs ou l’accès à des crypto-actifs (via des clés de chiffrement
-                            privées par exemple),l’achat/vente de crypto-actifs contre des monnaies ayant un cours légal (Euro, Dollar, etc.),
-                        </li>
-                        <li>
-                            L’échange de crypto-actifs contre d’autres crypto-actifs,
-                        </li>
-                        <li>
-                            Le placement garanti, sécurisé et sûr de crypto-actifs.
-                        </li>
-                        <li> L'exploitation de plateformes de négociation de crypto-actifs,</li>
-                        <li>La réception et la transmission d’ordre sur des crypto- actifs,</li>
-                        <li>La gestion de portefeuille de crypto- actifs,</li>
-                        <li>Le conseil aux investisseurs en crypto- actifs</li>
-                    </ul>
+                crypto-actifs.
             </p>
         
     </div>
+    </div>
+    <div class="w-11/12 flex justify-around">
+        <div class="md:w-3/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all w-11/12 overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/exchange.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Conservation</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 mb-2 font-semibold text-white grid place-items-center">1</span>
+                        <p class="w-11/12 mt-4 mx-auto">La conservation de crypto-actifs ou l’accès à des crypto-actifs (via des clés de chiffrement privées par exemple),l’achat/vente de crypto-actifs contre des monnaies ayant un cours légal (Euro, Dollar, etc.),</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/cryptos.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Echange</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 text-lg font-semibold text-white grid place-items-center">2</span>
+                        <p class="w-11/12 mt-4 mx-auto">L’échange de crypto-actifs contre d’autres crypto-actifs(BTC => ETH, DOGE => BTC, etc..) </p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/security.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Securité</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 text-lg font-semibold text-white grid place-items-center">3</span>
+                        <p class="w-11/12 mt-4 mx-auto">Le placement garanti, sécurisé et sûr de crypto-actifs.</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/explore.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Exploitation</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 text-lg font-semibold text-white grid place-items-center">4</span>
+                        <p class="w-11/12 mt-4 mx-auto">L'exploitation de plateformes de négociation de crypto-actifs</p>
+                    </div>
+
+                </div>
+
+            </div>
+    </div>
+    <div class="w-11/12 flex justify-around">
+        <div class="md:w-3/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all w-11/12 overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/give-money.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Réception et Transmission</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 mb-2 font-semibold text-white grid place-items-center">5</span>
+                        <p class="w-11/12 mt-4 mx-auto">La réception et la transmission d’ordre sur des crypto- actifs</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/wallet.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Gestion</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 text-lg font-semibold text-white grid place-items-center">6</span>
+                        <p class="w-11/12 mt-4 mx-auto">La gestion de portefeuille de crypto- actifs,</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer hover:bg-opacity-50 duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl relative shadow border border-gray-100 h-96">
+                <span class="w-full"><img src="/assets/logos/advice.jpg" alt="silver illustration" class="w-full z-0 object-cover h-40 mx-auto"></span>
+                <div class="text-gray-400 p-2">
+                    <div>
+                        <span class="_green_text text-lg ml-2 font-semibold">Conseil</span>
+                       <span class="w-8 h-8 z-20 absolute top-2 rounded-full _green_bg border-white border-2 text-lg font-semibold text-white grid place-items-center">7</span>
+                        <p class="w-11/12 mt-4 mx-auto">Le conseil aux investisseurs en crypto- actifs</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div class="md:w-3/12 w-11/12 transform hover:scale-125 cursor-pointer  duration-500 transition-all overflow-hidden m-4 primary_bg rounded-xl h-96">
+                
+            </div>
     </div>
 
 </div>
