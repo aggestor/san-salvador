19efe0dbd10a12c96b7d06abc7f9ad8d099976fb
Aggestor <aggeer.aa@gmail.com>
2021-11-25 15:19:58 +0100

    about & login - markup is ready with css - images are ready too - just need some texts

--- public/index.php
+++ public/index.php
@@ -17,6 +17,8 @@ $routes->get('/packages', 'Root\App\Controllers\StaticController@packages');
 $routes->get('/help', 'Root\App\Controllers\StaticController@help');
 $routes->get('/services', 'Root\App\Controllers\StaticController@service');
 $routes->get('/with-us', 'Root\App\Controllers\StaticController@with_us');
+$routes->get('/about', 'Root\App\Controllers\StaticController@about');
+$routes->get('/contact', 'Root\App\Controllers\StaticController@contact');
 try {
     $routes->run();
 } catch (NotFoundException $e) {
