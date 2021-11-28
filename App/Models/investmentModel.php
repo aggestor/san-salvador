32754f8c6b846d35068ebf4d1891bdce1293548d
mak098 <sergemakasi098@gmail.com>
2021-11-28 01:33:17 -0800

    methode get et getById dans le model investment

--- App/Models/investmentModel.php
+++ App/Models/investmentModel.php
@@ -34,5 +34,29 @@
             );
             return $query->rowCount();
         }
+        public function getById(array $param){
+            $schema=new Schema();
+            $investment=$schema->investment;
+            $table=$schema->DatabaseSchema;
+            $query=$this->find(
+                $table["investment"],
+                "*",
+               "{$investment["id"]}=?" ,
+                $param
+            );
+            return $query;
+        }
+        public function get(){
+            $schema=new Schema();
+            $investment=$schema->investment;
+            $table=$schema->DatabaseSchema;
+            $query=$this->find(
+                $table["investment"],
+                "*",
+               "{$investment["id"]}!=?" ,
+                ["@#$%^&**&^&^%$%#"]
+            );
+            return $query;
+        }
     }
 ?>
