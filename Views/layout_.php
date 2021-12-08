f1c568298d49c5b7855e2142906ad4a8aed7f9c7
Aggestor <aggeer.aa@gmail.com>
2021-12-08 19:51:38 +0100

    Changing add user design AGAIN

--- Views/layout_.php
+++ Views/layout_.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/tailwind.min.css" />
-    <link rel="stylesheet" href="assets/css/aos.min.css" />
-    <link rel="stylesheet" href="assets/css/global.css" />
-    <link rel="stylesheet" href="assets/fontawesome/css/all.min.css" />
+    <link rel="stylesheet" href="/assets/css/tailwind.min.css" />
+    <link rel="stylesheet" href="/assets/css/aos.min.css" />
+    <link rel="stylesheet" href="/assets/css/global.css" />
+    <link rel="stylesheet" href="/assets/fontawesome/css/all.min.css" />
     <!--
         <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     -->
@@ -19,9 +19,10 @@
 
     <?= $content ?>
 
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/js/aos.min.js"></script>
-    <script src="assets/js/main.js"></script>
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/aos.min.js"></script>
+    <script src="/assets/js/cropper.min.js"></script>
+    <script src="/assets/js/main.js"></script>
     <script>
         AOS.init()
     </script>
--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -38,7 +38,6 @@ toggleButton.addEventListener("click", () => {
 const timeHandler = (): void => {
   const time: Date = new Date
   const adminTimer: Element | null = document.querySelector("#adminTimer");
-  console.log(adminTimer)
   if (adminTimer) {
       adminTimer.textContent = `${time.getHours()} : ${time.getMinutes()}`;
   }
@@ -47,7 +46,9 @@ $(document).ready(() => {
   setInterval(() => timeHandler(), 10000);
 })
 
-
+/**
+ * Initializing cropper class
+ */
 const cropper = new Cropper({
   width: 320,
   height: 320,
@@ -57,7 +58,13 @@ const cropper = new Cropper({
   },
 });
 type stringable = string | null
-function dataURLtoFile(dataUrl:string, filename:string) {
+/**
+ * Covert base64 data image to Javascript object File
+ * @param dataUrl 
+ * @param filename 
+ * @returns {File | undefined}
+ */
+function dataURLtoFile(dataUrl:string, filename:string) : File | undefined {
   let array = dataUrl.split(",")
   if (array !== null) {
     const matchExtension: RegExpMatchArray | null = array[0].match(/data:(.*);/);
@@ -66,27 +73,57 @@ function dataURLtoFile(dataUrl:string, filename:string) {
     let mime : stringable
     
     if (matchExtension !== null) {
+
       extension = matchExtension[1].split("/")[1];
+
+      if (matchMimeType !== null) {
+
+        mime = matchMimeType[1]
+
+        let bstr = atob(array[1])
+        let n = bstr.length
+        let u8array : Uint8Array = new Uint8Array(n)
+      
+        while (n--) {
+          u8array[n] = bstr.charCodeAt(n);
+        }
+        return new File([u8array], filename + "." + extension, { type: mime });
+      }
     }
-    if (matchMimeType !== null) {
-      mime = matchMimeType[1]
-    }
-    let bstr = atob(array[1])
-    let n = bstr.length
-    let u8array : Uint8Array = new Uint8Array(n)
-  
-    while (n--) {
-      u8array[n] = bstr.charCodeAt(n);
-    }
-    return new File([u8array], filename + "." + extension, { type: mime });
   }
 }
 const imageUploader: HTMLInputElement | null = document.querySelector("#image")
 imageUploader !== null && imageUploader.addEventListener("change", (evt) => {
-  if(evt !== null)
-  const image = evt.target.files[0];
-  console.log(image);
-  //cropper.loadImage(imageURL)
+  if (evt.target !== null) {
+    const target : any = evt.target
+    
+    const image = target.files[0];
+  }
 });
 cropper.render("#crop");
 
+//Add user interactions stars here
+
+const formStepsButtons: NodeListOf<HTMLButtonElement> | null = document.querySelectorAll(".form-user-btn")
+formStepsButtons && formStepsButtons.forEach((button : HTMLButtonElement) => {
+  const buttonName: string = button.name
+  $(button).on("click", function (e : Event) {
+    e.preventDefault()
+    useFormButtons(buttonName)
+  })
+
+  function useFormButtons(name: string): void{
+    switch (name) {
+      case "2":
+        $(".form-1").slideUp()
+        $(".form-2").slideDown()
+        $(".form-3").slideUp()
+        break;
+      case "-2":
+        $(".form-1").slideDown()
+        $(".form-2").slideUp()
+        $(".form-3").slideUp()
+        break;
+    }
+  }
+})
