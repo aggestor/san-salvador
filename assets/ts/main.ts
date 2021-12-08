fd4a5b34310baba9e35ebd9a9133a5f5fee6a101
Aggestor <aggeer.aa@gmail.com>
2021-12-08 14:52:13 +0100

    My lastest changes

--- assets/ts/main.ts
+++ assets/ts/main.ts
@@ -48,3 +48,45 @@ $(document).ready(() => {
 })
 
 
+const cropper = new Cropper({
+  width: 320,
+  height: 320,
+  onChange: function () {
+    const image = this.getCroppedImage();
+    const file = dataURLtoFile(image, "test");
+  },
+});
+type stringable = string | null
+function dataURLtoFile(dataUrl:string, filename:string) {
+  let array = dataUrl.split(",")
+  if (array !== null) {
+    const matchExtension: RegExpMatchArray | null = array[0].match(/data:(.*);/);
+    const matchMimeType: RegExpMatchArray | null = array[0].match(/:(.*?);/);
+    let extension: stringable
+    let mime : stringable
+    
+    if (matchExtension !== null) {
+      extension = matchExtension[1].split("/")[1];
+    }
+    if (matchMimeType !== null) {
+      mime = matchMimeType[1]
+    }
+    let bstr = atob(array[1])
+    let n = bstr.length
+    let u8array : Uint8Array = new Uint8Array(n)
+  
+    while (n--) {
+      u8array[n] = bstr.charCodeAt(n);
+    }
+    return new File([u8array], filename + "." + extension, { type: mime });
+  }
+}
+const imageUploader: HTMLInputElement | null = document.querySelector("#image")
+imageUploader !== null && imageUploader.addEventListener("change", (evt) => {
+  if(evt !== null)
+  const image = evt.target.files[0];
+  console.log(image);
+  //cropper.loadImage(imageURL)
+});
+cropper.render("#crop");
+
