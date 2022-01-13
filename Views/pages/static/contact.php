646c87ecbdf33181f9e2d0e6372b4e5ea86acf17
Aggestor <aggeer.aa@gmail.com>
2022-01-13 09:44:40 +0100

    some changes in contacts whatsapp number

--- Views/pages/static/contact.php
+++ Views/pages/static/contact.php
@@ -5,7 +5,7 @@
             <img src="assets/logos/contact.png" class="md:h-96 h-80 w-80 md:w-96" alt="Joining image" />
         </div>
         <div data-aos="fade-down" data-aos-duration="1500" class="md:w-6/12 overflow-x-hidden w-11/12 lg:mx-0 mx-auto flex flex-col mt-6 ">
-            <h1 class="md:w-11/12 w-full lg:mx-0 mx-auto md:text-3xl text-xl m text-white font-bold">CONTACTEZ-NOUS</span></h1>
+            <h1 class="md:w-11/12 w-full lg:mx-0 mx-auto md:text-2xl text-xl text-white font-bold">CONTACTEZ-NOUS</span></h1>
             <p class="text-gray-500 my-2 md:w-11/12 w-full lg:mx-0 mx-auto">Laissez-nous un message en remplissant ce formulaire ci-dessous.</p>
             <form class="w-11/12 lg:mx-0 mx-auto flex flex-col">
                 <div class="lg:w-11/12 w-full focus-within:font-semibold text-gray-600 focus-within:text-green-500 group focus-within:border-green-500 mt-3 h-14 px-2 flex rounded-xl border-2 <?=$data = (isset($_POST['send']) && !empty($params['errors']['email'])) ? "border-red-500" : " border-gray-400"?>">
@@ -27,7 +27,7 @@
         </div>
     </div>
     <div class="lg:w-11/12 w-full flex h-60 items-center justify-center">
-       <p class="text-gray-300 text-center text-2xl lg:text-4xl"> <i class="fas fa-address-card    "></i> AUTRES CONTACTS</p>
+       <p class="text-gray-300 text-center text-2xl lg:text-3xl"> <i class="fas fa-address-card"></i> AUTRES CONTACTS</p>
     </div>
     <div class="w-11/12 mx-auto flex lg:flex-row flex-col space-x-5 mb-5 lg:h-96 ">
         <div class="lg:w-4/12 w-11/12 mx-auto lg:mx-0 mb-2 lg:mb-0 rounded border p-4 flex flex-col border-gray-900 h-80">
@@ -36,7 +36,7 @@
             </div>
             <div class="w-full grid place-items-center my-3">
                 <ul class="flex flex-col">
-                    <li class="hover:text-green-500 p-2 text-gray-400"><a href="tel:">+243 993 825 243</a></li>
+                    <li class="hover:text-green-500 p-2 text-gray-400"><a href="tel:">+1 323 678 5503</a></li>
                     <li class="hover:text-green-500 p-2 text-gray-400"><a href="tel:">+243 993 825 243</a></li>
                     <li class="hover:text-green-500 p-2 text-gray-400"><a href="tel:">+243 993 825 243</a></li>
                     <li class="hover:text-green-500 p-2 text-gray-400"><a href="tel:">+243 993 825 243</a></li>
@@ -70,7 +70,7 @@
                     <li class="hover:text-green-500 p-2 text-gray-400"><a class="flex w-full" href="#"><span class="mr-4 text-gray-500 grid place-items-center rounded-full"><i class="fab text-2xl fa-instagram"></i></span><span>Usalvagetrade</span></a></li>
                     <li class="hover:text-green-500 p-2 text-gray-400"><a class="flex w-full" href="#"><span class="mr-4 text-gray-500 grid place-items-center rounded-full"><i class="fab text-2xl fa-twitter"></i></span><span>uslavagetrade</span></a></li>
                     <li class="hover:text-green-500 p-2 text-gray-400"><a class="flex w-full" href="#"><span class="mr-4 text-gray-500 grid place-items-center rounded-full"><i class="fab text-2xl fa-youtube"></i></span><span>Usalvagetrade</span></a></li>
-                    <li class="hover:text-green-500 p-2 text-gray-400"><a class="flex w-full" href="#"><span class="mr-4 text-gray-500 grid place-items-center rounded-full"><i class="fab text-2xl fa-whatsapp"></i></span><span>+192329384940</span></a></li>
+                    <li class="hover:text-green-500 p-2 text-gray-400"><a class="flex w-full" href="#"><span class="mr-4 text-gray-500 grid place-items-center rounded-full"><i class="fab text-2xl fa-whatsapp"></i></span><span>+1 323 678 5503</span></a></li>
                 </ul>
             </div>
         </div>
