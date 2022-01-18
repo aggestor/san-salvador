5c6132613d9e2322cbbce0dd8ec9d1df8d3cae83
Aggestor <aggeer.aa@gmail.com>
2022-01-18 11:08:09 +0100

    redesing index cards

--- Views/includes/LandingThree.php
+++ Views/includes/LandingThree.php
@@ -1,11 +1,11 @@
 
 
-<div class="col-span-12 h-landing md:h-screen py-5 bg-gray-200">
+<div class="col-span-12 h-landing sm:h-auto py-5 bg-gray-200">
     <div  class="flex flex-col relative  w-11/12 mx-auto">
         <img data-aos="slide-up" data-aos-duration="1000" height="450"  src="assets/logos/pkg.jpg" class="md:w-8/12 overflow-x-hidden z-0  top-10 md:right-4 absolute" alt="Packages"/>
         <div data-aos="slide-up" data-aos-duration="1000" class="md:w-5/12 h-auto overflow-x-hidden  absolute py-8 px-4 md:top-24 top-56 md:left-48 primary_bg">
             <h3 class="pack_title mb-2 text-gray-400">AVEC NOUS</h3>
-            <p class="text-gray-400 my-3 _space_text_l3 md:w-10/12 ">Tout ce que vous découvrirez est une puissante plate-forme de trading qui vous permettra
+            <p class="text-gray-400 my-3  md:w-10/12 ">Tout ce que vous découvrirez est une puissante plate-forme de trading qui vous permettra
                 d'accéder à votre plein potentiel pour gagner un revenu passif stable.</br>
                 En tirant partie de Universal Salvage Trade, les entreprises ou les particuliers qui ont la difficulté
                 de faire ce genre de trading, peuvent facilement participer sans compétences ni connaissances...
