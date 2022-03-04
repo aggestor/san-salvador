0195e4e94fb5a20397cce631d4a423c9e1716a77
amaninyumu1 <amaninyumu1@gmail.com>
2022-03-04 15:47:00 +0100

    correction phone et evolution du compte

--- Views/pages/admin/login.php
+++ Views/pages/admin/login.php
@@ -2,7 +2,7 @@
     <div class="w-full h-20 flex flex-col items-center border-b border-gray-900">
         <div class="flex w-10/12 mx-auto mt-5">
             <a href="javascript:history.go(-1)" class="w-6 h-6 my-auto rounded-full grid place-items-center text-gray-900 bg-gray-300 mr-4"><i class="fas fa-arrow-left"></i></a>
-            <h1 class="text-gray-300 font-bold text-2xl text-left mx-a">USALVAGETRADE</h1>
+            <a href="/" class=" cursor-pointer"><h1 class="text-gray-300 hover:text-green-500 font-bold text-xl text-left mx-a">USALVAGETRADE</h1></a>
         </div>
         <h2 class="text-gray-400 w-10/12 font-semibold text-base mx-auto pl-10 text-left">Bienvenu(e) sur universal salvage trade</h2>
     </div>
--- Views/pages/admin/viewAllNotValidateCashout.php
+++ Views/pages/admin/viewAllNotValidateCashout.php
@@ -2,7 +2,7 @@
     <div class="col-span-12 flex flex-col">
         <div class="w-11/12 mx-auto mb-4 h-12 border-b border-gray-900 flex justify-between">
             <h1 class="text-gray-300 font-semibold text-xl">Toutes les demandes de retrait en attente.</h1>
-            <span class="bg-blue-500 text-white w-8 h-8 rounded-full grid place-items-center" ><?= count($params['cashOut']) ?></span>
+            <span class="bg-blue-500 text-white w-8 h-8 rounded-full grid place-items-center"><?= count($params['cashOut']) ?></span>
         </div>
         <div class="w-11/12 mx-auto p-3 text-gray-300  mt-6 mb-3 border border-gray-700 primary_bg_ flex justify-between rounded">
             <div class="w-1/12">Noms</div>
@@ -12,15 +12,17 @@
             <div class="w-1/12">Date</div>
             <div class="w-1/12">Action</div>
         </div>
-        <?php foreach($params['cashOut'] as $data) :?>
-        <div class="w-11/12 mx-auto p-1 items-center text-gray-500  my-2 border border-gray-500 flex justify-between rounded">
-            <div class="w-1/12"><?= $data->getUser()->getName()?></div>
-            <div class="w-2/12"><?= $data->getUser()->getPhone()?></div>
-            <div class="w-2/12"><?= $data->getUser()->getEmail()?></div>
-            <div class="w-1/12 font-semibold text-white"><?= $data->getAmount()?> USD</div>
-            <div class="w-1/12"><?= $data->getRecordDate()->format("d-m-Y")?></div>
-            <div><form method="POST" action="/admin/validate/cashout-<?= $data->getId()."-".$data->getUser()->getId() ?>"><button class="_green_bg rounded text-gray-800 p-1.5" type="submit">Valider <i class="fas fa-check-circle    "></i></button></form></div>
-        </div>
-        <?php endforeach;?>
+        <?php foreach ($params['cashOut'] as $data) : ?>
+            <div class="w-11/12 mx-auto p-1 items-center text-gray-500  my-2 border border-gray-500 flex justify-between rounded">
+                <div class="w-1/12"><?= $data->getUser()->getName() ?></div>
+                <div class="w-2/12"><?= str_replace("/", "", $data->getUser()->getPhone()) ?></div>
+                <div class="w-2/12"><?= $data->getUser()->getEmail() ?></div>
+                <div class="w-1/12 font-semibold text-white"><?= $data->getAmount() ?> USD</div>
+                <div class="w-1/12"><?= $data->getRecordDate()->format("d-m-Y") ?></div>
+                <div>
+                    <form method="POST" action="/admin/validate/cashout-<?= $data->getId() . "-" . $data->getUser()->getId() ?>"><button class="_green_bg rounded text-gray-800 p-1.5" type="submit">Valider <i class="fas fa-check-circle    "></i></button></form>
+                </div>
+            </div>
+        <?php endforeach; ?>
     </div>
 </div>
